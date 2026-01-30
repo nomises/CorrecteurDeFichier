0f1dee3b069d19dba651e6e61c63e9d12d62d8bd
nomises <nomises@github.com>
2026-01-29 20:45:49 -0500

    Installation de maven

---
+++ src/main/java/Const.java
@@ -0,0 +1,10 @@
+import java.nio.file.Paths;
+
+public class Const {
+    static String PATH = Paths.get("").toAbsolutePath().toString();
+    static String PATH_RESULTAT = PATH + "\\" + "_resultat.txt";
+    static String TEMP = "TEMP_";
+    static String SEPARATEUR = "----------------------------------------------";
+    static char LF = '\n';
+    static String CRLF = "\r\n";
+}
